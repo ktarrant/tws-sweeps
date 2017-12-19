763818d2d6a6bd677bd67dc423b2d840d83e0821
Kevin Tarrant <kmtarrant13@gmail.com>
2017-12-18 22:58:03 -0500

    build out statement testing

--- discussion.py
+++ discussion.py
@@ -18,16 +18,16 @@ month_options = [
     ("Week|week", "ly|lies")
 ]
 sweep_elements = OrderedDict([
-    ("ticker", (r"[A-Z]{2,5}", ["ticker"])),
+    ("ticker", (r"(^[A-Z][A-Za-z]{1,3})|[A-Z]{0-4}", ["ticker", "_ticker"])),
     ("expiration",  ("|".join(["{}(?:{})?".format(prefix, suffix) for prefix, suffix in month_options]),
                      ["expiration"])),
-    ("strike", ((r"(c|p|C|P)?([1-9][0-9]{0,3})(c|p|C|P)?"), ["_num", "option_p", "num", "option_s"])),
+    ("strike", ((r"(c|p|C|P)?([1-9][0-9]{0,3})(c|p|C|P)?"), ["_strike", "option_p", "strike", "option_s"])),
     ("option", (r"call|put", ["option_e"])),
 ])
 sweep_elements_re_keys = [key for element in sweep_elements for key in sweep_elements[element][1]]
 sweep_elements_re = re.compile("|".join(["(" + sweep_elements[key][0] + ")" for key in sweep_elements]))
 
-def _parse_comment_content(content):
+def parse_comment_content(content):
     """
     :param content: str
     :yield: OrderedDict's each representing a statement in the comment
@@ -37,20 +37,22 @@ def _parse_comment_content(content):
         log.debug("[Processing Statement] {}".format(statement))
         sweep_elements_matches = sweep_elements_re.findall(statement)
 
-        combined_result = OrderedDict()
+        entry = {}
         for match in sweep_elements_matches:
-            result = {key: value for key, value in zip(sweep_elements_re_keys, match)}
-            for key in result:
-                if result[key] != '':
-                    combined_result[key] = result[key]
+            for re_key, value in zip(sweep_elements_re_keys, match):
+                if re_key.startswith("_") or value == "":
+                    continue
 
-        if len(combined_result.keys()) > 0:
-            yield combined_result
+                s_key = re_key.split("_")[0]
+                entry[s_key] = value
+
+        yield entry
+
 
 def parse_comments(submission):
     submission.comments.replace_more(limit=None)
     for comment in submission.comments.list():
-        for combined_result in _parse_comment_content(comment.body):
+        for combined_result in parse_comment_content(comment.body):
             if all([k in combined_result for k in ["ticker", "expiration", "num"]]):
                 log.info("[Found Sweep] {}".format(combined_result))
                 yield combined_result
--- test_discussion.py
+++ test_discussion.py
@@ -2,7 +2,7 @@ import praw
 import pytest
 import logging
 
-from discussion import _parse_comment_content, parse_comments
+from discussion import parse_comment_content, parse_comments
 
 from credentials import CLIENT_ID, CLIENT_SECRET
 
@@ -28,21 +28,39 @@ statement_examples = [
     "TSLA Jan C330 buyer 1k contracts 10.85-11.05 for ~$1.1mm prem",
     "JD May C40 buyer of 580 contracts @4.",
     "AMD weekly C12 & C12.5 going.",
-    "20k contracts Jan C12/C13 spread went off",
-    "ATVI $62.5C and $65C have lots of volume as well",
     "DDD Weekly Nov10 9 calls",
     "AABA Dec 80 calls",
     "ATVI Dec 52.5 calls",
     "NRG Nov 27 puts",
-    "edit: what a joke. 5k contracts Nov C20 swept this morning at .3",
     "Baba Jan 190s hit twice for $1.5m call sweeps",
     "Qqq 12/8 152 put sweep for $475k",
 ]
 
-@pytest.mark.parametrize("statement", statement_examples)
-def test_statement_examples(statement):
-    result = list(_parse_comment_content(statement))
+statement_results = [
+    [dict(ticker="TSLA",    strike=330.0,   expiration="Jan",       option="call")],
+    [dict(ticker="JD",      strike=40.0,    expiration="May",       option="call")],
+    [
+        dict(ticker="AMD", strike=12.0, expiration="Weekly", option="call"),
+        dict(ticker="AMD", strike=12.5, expiration="Weekly", option="call"),
+    ],
+    [dict(ticker="DDD",     strike=9.0,     expiration="Weekly",    option="call")],
+    [dict(ticker="AABA",    strike=80.0,    expiration="Dec",       option="call")],
+    [dict(ticker="ATVI",    strike=62.5,    expiration="Weekly",    option="call")],
+    [dict(ticker="NRG",     strike=27.0,    expiration="Nov",       option="put")],
+    [dict(ticker="BABA",    strike=190.0,   expiration="Jan",       option="call")],
+    [dict(ticker="QQQ",     strike=152.0,   expiration="12/8",      option="put")],
+]
+
+def check_statement_result(entry, expected_entry):
+    for key in expected_entry:
+        assert expected_entry[key] == entry[key]
+
+@pytest.mark.parametrize("statement,expected_result", list(zip(statement_examples, statement_results)))
+def test_statement_examples(statement, expected_result):
+    result = list(parse_comment_content(statement))
     log.info(result)
+    for (entry, expected_entry) in zip(result, expected_result):
+        check_statement_result(entry, expected_entry)
 
 def test_sample_discussion_parse_comments(sample_discussion):
     result = list(parse_comments(sample_discussion))
