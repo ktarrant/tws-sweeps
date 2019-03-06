0d79c481ae01a1ad6d7c479ad3ffdbe98ee8710a
Kevin Tarrant <kmtarrant13@gmail.com>
2019-03-06 01:29:13 -0500

    dont remember what this is

---
+++ samples.py
@@ -0,0 +1,25 @@
+comments_lembas = {
+    "Jesus twx July 95 call block $5.5M. alright twx you have my attention": [
+        {"ticker": "TWX", "expiration": "July", "option": "call", "strike": 95.0, "value": "$5.5M"},
+    ],
+    """Chtr Feb 330 call sweep $498k. Largest position in strike and volume is greater than OI.
+Hlt Feb 80 call sweeps. Hlt calls stacking now seen several today. $230k this one.
+Xom Jan 83 call block $257k. Watching XOM closely....
+Wmt March 92.5 call sweep $397k""": [
+        {"ticker": "CHTR", "expiration": "Feb", "option": "call", "strike": 330.0, "value": "$498k"},
+        {"ticker": "HLT", "expiration": "Feb", "option": "call", "strike": 80.0, "value": "$230k"},
+        {"ticker": "XOM", "expiration": "Jan", "option": "call", "strike": 83.0, "value": "$257k"},
+        {"ticker": "WMT", "expiration": "March", "option": "call", "strike": 92.5, "value": "$397k"},
+    ],
+    """Damn mother fucking spy.
+Feb 267 calls and Jan 263 calls hit for $1.7M and $1.3M.
+Qqq Feb 159 call block $831k.
+Huge put play on PCG Jan 50s for $6.9M.
+Also amzn 12/29 1175 calls hit again for $546k""": [
+        {"ticker": "SPY", "expiration": "Feb", "option": "call", "strike": 267.0, "value": "$1.7M"},
+        {"ticker": "SPY", "expiration": "Feb", "option": "call", "strike": 263.0, "value": "$1.3M"},
+        {"ticker": "QQQ", "expiration": "Feb", "option": "call", "strike": 159.0, "value": "$831k"},
+        {"ticker": "PCG", "expiration": "Jan", "option": "put", "strike": 50.0, "value": "$6.9M"},
+        {"ticker": "AMZN", "expiration": "12/29", "option": "call", "strike": 1175.0, "value": "$546k"},
+    ],
+}
